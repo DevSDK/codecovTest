6c8fd4472858f5a7864975374b4b76fb8d344f14
Seokho Song <seokho@chromium.org>
2023-04-05 09:17:11 +0900

    Add some functions

--- libs/add.ts
+++ libs/add.ts
@@ -1,3 +1,33 @@
 export function add(a: number, b: number) {
   return a + b;
 }
+
+export function addWithBranch(a: number, b: number, condition: boolean) {
+  if (condition) {
+    return a + a + b + b;
+  }
+  return a + b;
+}
+
+export type AddString = {
+  type: "string";
+  left: string;
+  right: string;
+};
+
+export type AddNumber = {
+  type: "number";
+  left: number;
+  right: number;
+};
+
+export function addWithSwitch(p: AddString | AddNumber) {
+  switch (p.type) {
+    case "number":
+      return p.left + p.right;
+    case "string":
+      return `${p.left} + ${p.right}`;
+    default:
+      return null;
+  }
+}
