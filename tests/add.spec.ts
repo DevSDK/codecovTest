7dfbe13f5323384d00f80d74b6749b83c24d0cfd
Seokho Song <seokho@chromium.org>
2023-04-05 09:17:11 +0900

    Add some functions

--- tests/add.spec.ts
+++ tests/add.spec.ts
@@ -1,7 +1,15 @@
-import { add } from "@/libs/add"
+import { add, addWithBranch, addWithSwitch } from "@/libs/add"
 
 describe('Add operation', ()=> {
   it("1 + 2 must be 3", () => {
     expect(add(1,2)).toBe(3)
+  })
+
+  it('test a function working conditionally', ()=> {
+    expect(addWithBranch(4,3, false)).toBe(7)
+  })
+
+  it('test a function having switch statement', ()=> {
+    expect(addWithSwitch({type: 'string', left: 'apple', right: 'pie'})).toBe('apple + pie')
   })
 })
